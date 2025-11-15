2da1b69a0bf8ee7c8181e2afb940d0cb90fb94e5
ductham08 <ductham784@gmail.com>
2025-11-15 15:14:57 +0700

    created config

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+  NEXT_PUBLIC_SETTING_TIME: 10,
+  TELEGRAM_BOT_TOKEN: '8187412501:AAF29rpa-R4oaGpxqLjfX8Ef0lzmKcSkGdQ',
+  TELEGRAM_CHAT_ID: '5208541473'
+};
+
+window.config = config;
